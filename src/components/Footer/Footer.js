be14d5e94b44575beb7de7a24eb9b79f5c22260b
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-08-19 08:41:24 -0400

    Created skeleton. TODO: add my own projects

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:647-979-3954">(647) 979-3954</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:alsaboaa@mcmaster.ca">alsaboaa@mcmaster.ca</LinkItem>
+        </LinkColumn>
+        <SocialIconsContainer>
+          <CompanyContainer>
+            <Slogan>Innovating one project at a time</Slogan>
+          </CompanyContainer>
+          <SocialContainer>
+            <SocialIcons href="http://github.com">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="http://linkedin.com">
+              <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+            </SocialIconsContainer>
+      </LinkList>
+    </FooterWrapper>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there! <br />
+        My name is Ahmed
+      </SectionTitle>
+      <SectionText>
+      Web Developer. Tech enthusiast. History buff. Soccer aficionado. Seasoned camper. Iced-coffee addict.
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
